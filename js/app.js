6574b9ed5159e9414a6bc053f2321dfcd0279ce8
Matias110 <40966311+Matias110@users.noreply.github.com>
2021-01-26 15:15:23 -0300

    Loaded Page

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const clausContainer = document.querySelector('.claus-container');
+
+  //loaded Page
+  /*window.addEventListener('load', () => {
+    clausContainer.style.display = 'none';
+  })*/
+
